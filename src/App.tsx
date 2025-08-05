d6fe4f6836abb58e47e1868e5b695a688e1d1d6f
Subham9436 <subham6531@gmail.com>
2025-08-05 11:42:02 +0530

    firstanimation

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import "./App.css";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 import gsap from "gsap";
+import { Navbar } from "./components/navbar";
+import { Hero } from "./components/hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
-  return <div className="text-3xl text-indigo-300 flex-center h-[100vh]">GSAP YAY!!!</div>;
+  return <main>
+    <Navbar/>
+    <Hero/>
+    <div className="h-dvh bg-black">HIII</div>
+  </main>
 }
 
 export default App;
